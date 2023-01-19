d1810c24aa87e60d54d607cb2c525c57031fac4e
desertyou <and18ste@gmail.com>
2023-01-19 16:21:30 +0100

    time spent

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -6,9 +6,16 @@ var charsTyped = 0;
 window.onload = function(){
     var form = document.getElementsByTagName("body")[0];
     var disp = document.getElementById("display");
+    var spent = document.getElementById("timeSpent");
     
+
+    var start=Date.now();
     form.onclick = function () {
         mouseClicks++;
         disp.innerHTML = mouseClicks;
     }
+    setInterval(() => {
+        let elapsed = Math.round((Date.now() - start) / 1000);
+        spent.innerHTML(elapsed);
+      }, 1000);
 }
