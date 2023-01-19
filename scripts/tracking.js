204b4b356aa55fbc33eb1cceca52e79e3fb5abc0
desertyou <and18ste@gmail.com>
2023-01-19 17:31:49 +0100

    comments

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -3,7 +3,9 @@ var timeSpent = 0;
 var keyPresses = 0;
 var charsTyped = 0;
 var isSubmitPressed = false;
-
+/**
+ * Once the window has loaded, we run this code:
+ */
 window.onload = function () {
     var results = document.getElementById("results");
     results.style.visibility = "hidden";
@@ -18,10 +20,18 @@ window.onload = function () {
     var elapsed;
     let charsTyped = 0;
     var forms = document.getElementById("realForm");
+    /**
+     * To not refresh the page after the submit button is pressed.
+     */
     forms.addEventListener("submit", function (event) {
         event.preventDefault();
     });
-
+    /**
+     * When the submit button has been clicked:
+     * 1. checks the length of each input field to determine the
+     * amount of characters typed
+     * 2. shows the div containing all the tracking info.
+     */
     subBut.addEventListener("click", function () {
         results.style.visibility = "visible";
         charsTyped=0;
@@ -31,13 +41,21 @@ window.onload = function () {
         chars.innerHTML = charsTyped;
         isSubmitPressed = true;
     });
-
+    /**
+     * Checks how many mouse clicks have occurred on the input form.
+     * Once the submit button has been pressed, the mouse click counter stops.
+     */
     form.onclick = function () {
         if (!isSubmitPressed) {
             mouseClicks++;
         }
         disp.innerHTML = mouseClicks;
     }
+    /**
+     * Every second, we update the timer which tracks the amount
+     * of time spent by the user on the page.
+     * Once the submit button has been pressed, the tracking stops.
+     */
     setInterval(() => {
         if (!isSubmitPressed) {
             elapsed = Math.round((Date.now() - startTime) / 1000);
@@ -51,7 +69,11 @@ window.onload = function () {
             spent.innerHTML = minutes + " minutes, " + seconds + " seconds";
     }, 1000);
 
-
+    /**
+     * Checks when a key has been pressed and updates the keypress counter accordingly;
+     * If the submit button has been pressed already, we do not
+     * track any other key presses.
+     */
     document.addEventListener("keydown", function () {
         if (!isSubmitPressed) {
             keyPresses++;
