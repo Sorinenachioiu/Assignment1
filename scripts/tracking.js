a36d5031e052e5a46bdf24800b446010ede9d13e
Enachioiu Sorin <sorinenachioiu@yahoo.com>
2023-01-19 16:56:20 +0100

    done hidden

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -2,24 +2,40 @@ var mouseClicks = 0;
 var timeSpent = 0;
 var keyPresses = 0;
 var charsTyped = 0;
-
+var isSubmitPressed = false;
 
 window.onload = function(){
+    var results = document.getElementById("results");
+    results.style.visibility = "hidden";
     var form = document.getElementsByTagName("body")[0];
     var disp = document.getElementById("display");
     var spent = document.getElementById("timeSpent");
     var keys = document.getElementById("keyPresses");
+    var subBut = document.getElementById("subButSignUp");
     var startTime = Date.now();
+    var elapsed;
+    var forms = document.getElementById("realForm");
+        forms.addEventListener("submit", function(event) {
+        event.preventDefault();    
+    });
 
-    var start=Date.now();
+    subBut.addEventListener ("click", function(){
+        results.style.visibility = "visible";
+        isSubmitPressed = true;
+    });
+
     form.onclick = function () {
-        mouseClicks++;
+        if(!isSubmitPressed){
+            mouseClicks++;   
+        }
         disp.innerHTML = mouseClicks;
     }
     setInterval(() => {
-        let elapsed = Math.round((Date.now() - startTime) / 1000);
-        let minutes = Math.floor(elapsed/60);
-        let seconds = elapsed%60;
+        if(!isSubmitPressed){
+            elapsed = Math.round((Date.now() - startTime) / 1000);
+        }
+        var minutes = Math.floor(elapsed/60);
+        var seconds = elapsed%60;
         if(minutes==0){
             spent.innerHTML = seconds + " seconds";
         }
@@ -29,7 +45,10 @@ window.onload = function(){
 
       
     document.addEventListener("keydown", function() {
-        keyPresses++;
+        if(!isSubmitPressed){
+            keyPresses++;
+        }
         keys.innerHTML = keyPresses;
     });
+
 }
